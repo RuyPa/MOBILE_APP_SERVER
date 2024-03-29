65ab550f0937d4bd1a36f3fdcf79e16cebf866f7
unknown <dobaduy.hungyen@gmail.com>
2024-03-29 09:09:22 +0700

    init

---
+++ target/classes/mobile_app_server.sql
@@ -0,0 +1,85 @@
+use sql6693869;
+
+-- create table tblUser (
+-- 	id int auto_increment primary key
+-- );
+
+-- create table tblEvent(
+-- 	id int auto_increment primary key,
+--     userId int,
+--     foreign key (userId) references tblUser(id)
+-- );
+
+-- create table tblCategory(
+-- 	id int auto_increment primary key
+-- );
+
+-- create table tblEventCategory(
+-- 	id int auto_increment primary key,
+--     eventId int,
+--     categoryId int,
+--     foreign key (eventId) references tblEvent(id),
+--     foreign key (categoryId) references tblCategory(id)
+-- );
+
+-- create table tblTask(
+-- 	id int auto_increment primary key,
+--     eventId int,
+--     foreign key (eventId) references tblEvent(id)
+-- );
+
+-- create table tblSchedule(
+-- 	id int auto_increment primary key,
+--     eventId int,
+--     foreign key (eventId) references tblEvent(id)
+-- );
+
+-- alter table tblCategory
+-- add name varchar(255);
+
+-- alter table tblCategory
+-- add des varchar(255);
+
+-- alter table tblEvent
+-- add name varchar(255);
+
+-- alter table tblEvent
+-- add date varchar(255);
+
+-- alter table tblEvent
+-- add starttime varchar(255);
+
+-- alter table tblEvent
+-- add endtime varchar(255);
+
+-- alter table tblEvent
+-- add location varchar(255);
+
+-- alter table tblEvent
+-- add address varchar(255);
+
+-- alter table tblEvent
+-- add city varchar(255);
+
+-- alter table tblEvent
+-- add des varchar(255);
+
+-- alter table tblEvent
+-- add eventvideo varchar(255);
+
+-- alter table tblEvent
+-- add registrationtype varchar(255);
+
+-- alter table tblEvent
+-- add websitelink varchar(255);
+
+-- alter table tblEvent
+-- add imgurl varchar(255);
+
+-- alter table tblEvent
+-- add startdate date;
+
+-- alter table tblEvent
+-- add enddate date;
+
+
